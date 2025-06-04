44973c3a4b1ccba4adb9605c1cb24fcf54acba0e
Samus Dimitriy <115738100+samusdimitriy@users.noreply.github.com>
2025-06-04 23:30:03 +0200

    Add basic yacht rental app skeleton

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { lazy } from 'react';
 
 export const routes = [
-  { path: '/',         element: lazy(() => import('./pages/Login')) },
-  // { path: '/dates',    element: lazy(() => import('./pages/SelectDates')) },
-  // { path: '/yachts',   element: lazy(() => import('./pages/AvailableYachts')) },
-  // { path: '/yacht/:id',element: lazy(() => import('./pages/YachtDetails')) },
-  // { path: '/profile',  element: lazy(() => import('./pages/Profile')) },
-  // { path: '/profile/edit', element: lazy(() => import('./pages/EditProfile')) },
+  { path: '/', element: lazy(() => import('./pages/Login')) },
+  { path: '/register', element: lazy(() => import('./pages/Register')) },
+  { path: '/dates', element: lazy(() => import('./pages/SelectDates')) },
+  { path: '/yachts', element: lazy(() => import('./pages/AvailableYachts')) },
+  { path: '/yacht/:id', element: lazy(() => import('./pages/YachtDetails')) },
+  { path: '/saved', element: lazy(() => import('./pages/Saved')) },
+  { path: '/profile', element: lazy(() => import('./pages/Profile')) },
+  { path: '/profile/edit', element: lazy(() => import('./pages/EditProfile')) },
 ];
